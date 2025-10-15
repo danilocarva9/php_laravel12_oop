f2a6083b960410848f33286a5ad37cebdbac9d0d
Danilo Carvalho <danilocarva9@gmail.com>
2025-10-15 11:49:29 -0400

    on going

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/health', function () {
-    return ['welcome' => 'Hello World'];
+    return response()->json(['welcome' => 'Hello World']);
 });
